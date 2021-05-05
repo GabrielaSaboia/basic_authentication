811794bc0891da4495ac2487df326af8c6b16ea6
gabrielasaboia <gs525@njit.edu>
2021-05-05 16:33:22 -0400

    implementation of authorization tokens

--- src/app.js
+++ src/app.js
@@ -44,6 +44,10 @@ const getHashedPassword = (password) => {
     return hash;
 }
 
+const generateAuthToken = () => {
+    return crypto.randomBytes(30).toString('hex');
+}
+
 const users = [
     // This user is added to the array to avoid creating a new user on each restart
     {
@@ -89,6 +93,37 @@ app.post('/register', (req, res) => {
     } else {
         res.render('register', {
             message: 'Password does not match.',
+            messageClass: 'alert-danger'
+        });
+    }
+});
+
+
+// This will hold the users and authToken related to users
+const authTokens = {};
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const hashedPassword = getHashedPassword(password);
+
+    const user = users.find(u => {
+        return u.email === email && hashedPassword === u.password
+    });
+
+    if (user) {
+        const authToken = generateAuthToken();
+
+        // Store authentication token
+        authTokens[authToken] = user;
+
+        // Setting the auth token in cookies
+        res.cookie('AuthToken', authToken);
+
+        // Redirect user to the protected page
+        res.redirect('/protected');
+    } else {
+        res.render('login', {
+            message: 'Invalid username or password',
             messageClass: 'alert-danger'
         });
     }
